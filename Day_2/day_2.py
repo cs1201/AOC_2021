7aa4ad338f6ef5d41f9d73b598f76b00db480808
cs1201 <ronmst@aol.com>
2021-12-02 10:42:27 +0000

    Day 2 - Initial

---
+++ Day_2/day_2.py
@@ -0,0 +1,37 @@
+import sys
+sys.path.insert(1, '../util')
+from profiler import profile
+
+@profile
+def part_one(data):
+    x, y = 0, 0
+    for d, m in data:
+        if d == "forward":
+            x += m
+        if d == "up":
+            y -= m
+        if d == "down":
+            y += m
+    return x * y
+
+@profile
+def part_two(data):
+    x, y, a = 0, 0, 0
+    for d, m in data:
+        if d == "forward":
+            x += int(m)
+            if a:
+                y += int(m) * a
+        if d == "up":
+            a -= m
+        if d == "down":
+            a += m
+    return x * y
+
+with open("day_2_input.txt") as f:
+    data = [(line.strip().split()[0], int(line.strip().split()[1])) for line in f.readlines()]
+    print(f"Part One: {part_one(data)}") # 1654760
+    print(f"Part Two: {part_two(data)}") # 1956047400
+
+    # assert(part_one(data) == 1654760)
+    # assert(part_two(data) == 1956047400)
