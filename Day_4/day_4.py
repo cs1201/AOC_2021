0ca8b8ae556c0a84c88f669d16df683d322d7a1f
cs1201 <ronmst@aol.com>
2021-12-04 13:42:05 +0000

    Day 4 - python refactor

--- Day_4/day_4.py
+++ Day_4/day_4.py
@@ -5,58 +5,29 @@ from profiler import profile
 def flipped(l):
     return list(map(list, zip(*l)))
 
-def check_number(board, draw):
-    for i, row in enumerate(board):
-        for j, _ in enumerate(row):
-            if board[i][j][0] == draw:
-                board[i][j] = (board[i][j][0], True)
-    return board
+def mark_number(board, draw):
+    return [[(num, True if num == draw else marked) for num,marked in row] for row in board]
 
 def bingo(board):
-    for row in board:
-        if all(x for _,x in row):
-            return True
-    for col in flipped(board):
-        if all(x for _,x in col):
-            return True
-    return False
+    return any(all([marked for _,marked in row]) for row in board) or \
+           any(all([marked for _,marked in col]) for col in flipped(board))
 
 def sum_unmarked(board):
     return sum(num for row in board for num,marked in row if not marked)
 
 @profile
-def part_one(draw, boards):
-    # print(draw)
-    for num in draw:
-        for i, board in enumerate(boards):
-            boards[i] = check_number(board, num)
-            if bingo(board):
-                return sum_unmarked(board) * num
-
-@profile
-def part_two(draw, boards):
+def play(draw, boards):
     winning_boards = []
-    already_won = []
     for num in draw:
-        for i, board in enumerate(boards):
-            boards[i] = check_number(board, num)
-            if bingo(board) and (i not in already_won):
-                already_won.append(i)
-                winning_boards.append((sum_unmarked(board) * num))
-    return winning_boards[-1]
+        for i, _ in enumerate(boards):
+            boards[i] = mark_number(boards[i], num)
+            if bingo(boards[i]) and (i not in [x for _,x in winning_boards]):
+                winning_boards.append(((sum_unmarked(boards[i]) * num),i))
+    return [res for res,_ in winning_boards]
 
 with open("day_4_input.txt") as f:
-    input_f = f.readlines()
-    draw = list(map(int, input_f[0].split(',')))
-    boards = []
-    board = []
-    for line in input_f[1:]:
-        if line != "\n" and line != "":
-            board.append([(int(x), False) for x in line.split()])                                                
-        else:
-            if len(board):
-                boards.append(board)
-                board = []
-
-    print(f"Part One: {part_one(draw, boards)}") # 39984
-    print(f"Part Two: {part_two(draw, boards)}") #  8468
+    draw = list(map(int, f.readline().strip().split(',')))
+    boards = [[[(int(x), False) for x in row.split()] for row in board.split("\n")] for board in f.read().strip().split("\n\n")]
+    winners = play(draw, boards)
+    print(f"Part One: {winners[0]}") # 39984
+    print(f"Part Two: {winners[-1]}") #  8468
