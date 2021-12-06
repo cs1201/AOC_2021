6d0b9aa83ef65e53ca2bbe4082a7231099ca8795
cs1201 <ronmst@aol.com>
2021-12-06 16:15:21 +0000

    Day 6

--- Day_6/day_6.py
+++ Day_6/day_6.py
@@ -3,6 +3,7 @@ sys.path.insert(1, '../util')
 from profiler import profile
 from collections import Counter
 
+@profile
 def model_lf(days, data):
     for _ in range(days):
         zeros = data.pop(0)
@@ -11,16 +12,8 @@ def model_lf(days, data):
             data[6] += zeros
     return sum(data)
 
-@profile
-def part_one(days, data):
-    return model_lf(days, data)
-
-@profile
-def part_two(days, data): 
-    return model_lf(days,data)
-
 with open("day_6_input.txt") as f:
     c = Counter(list(map(int, f.read().split(','))))
     data = [c[i] for i in range(9)]
-    print(f"Part One: {part_one(80, data[:])}") # 386536
-    print(f"Part Two: {part_two(256, data[:])}") # 1732821262171
+    print(f"Part One: {model_lf(80, data[:])}") # 386536
+    print(f"Part Two: {model_lf(256, data[:])}") # 1732821262171
